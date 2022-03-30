6de6b91835d5aac3b5a755d824528adfd4d01e2b
Gustavo Vieira <guga9055@gmail.com>
2022-03-30 18:48:44 -0300

    Teste na tela about

--- api/navbar.js
+++ api/navbar.js
@@ -1,18 +1,18 @@
-
-const btnMobile = document.getElementById('btn-mobile');
+ const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
-  const nav = document.getElementById('container-btn-mobile');
-  nav.classList.toggle('active');
-  const active = nav.classList.contains('active');
-  event.currentTarget.setAttribute('aria-expanded', active);
-  if (active) {
-    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
-  } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
-  }
-}
+        const nav = document.getElementById('nav');
+        nav.classList.toggle('active');
+        const active = nav.classList.contains('active');
 
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+        event.currentTarget.setAttribute('aria-expanded', active);
+        if (active) {
+            event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+        } else {
+            event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+        }
+    }
+
+    btnMobile.addEventListener('click', toggleMenu);
+    btnMobile.addEventListener('touchstart', toggleMenu);
