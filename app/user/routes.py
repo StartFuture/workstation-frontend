9677308d982f6b46714ee1d0523af7252268a96f
Lucas Nunes <64300982+kilerhg@users.noreply.github.com>
2022-04-04 09:57:47 -0300

    Creating Basic Skeleton of endpoint
    Creating endpoints that will be importart after

--- app/user/routes.py
+++ app/user/routes.py
@@ -4,7 +4,50 @@ from user import bp
 
 @bp.route('/')
 def index():
-    return 'teste route user'
+    return redirect(url_for('main.index'))
+
+
+@bp.route('/login', methods=['GET', 'POST'])
+def login():
+    
+    if request.method == 'POST':
+        return jsonify({'status': 'ok'})
+    
+    elif request.method == 'GET':
+        return render_template('user/login.html')
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    
+    if request.method == 'POST':
+        return jsonify({'status': 'ok'})
+    
+    elif request.method == 'GET':
+        return render_template('user/register.html')
+    
+    
+@bp.route('/reset_password', methods=['GET', 'POST'])
+def reset_password():
+    
+    if request.method == 'POST':
+        return jsonify({'status': 'ok'})
+    
+    elif request.method == 'GET':
+        # return render_template('user/reset_password.html')
+        return 'reset_password'
+        
+    
+@bp.route('/valid_code', methods=['GET', 'POST'])
+def valid_code():
+    
+    if request.method == 'POST':
+        return jsonify({'status': 'ok'})
+    
+    elif request.method == 'GET':
+        # return render_template('user/valid_code.html')
+        return 'valid_code'
+        
 
 
 
