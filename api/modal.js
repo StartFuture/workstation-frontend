7adebae63854567be751becf7bca2b09dbdd2dc0
Gustavo Vieira <guga9055@gmail.com>
2022-04-10 03:18:11 -0300

    Modal sendo ajustado em todas as telas

--- api/modal.js
+++ api/modal.js
@@ -9,23 +9,23 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
-  modal.style.display = "block";
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-  modal.style.display = "none";
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 var closeBtn = document.getElementsByClassName("btn-continue-cadastro")[0];
 
-closeBtn.onclick = function(){
-   modal.style.display = "none";
+closeBtn.onclick = function() {
+    modal.style.display = "none";
 }
