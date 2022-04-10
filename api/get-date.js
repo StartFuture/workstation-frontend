214a3bd134e7046350b0ed5d1044f8234c4bf8a1
Gustavo Vieira <guga9055@gmail.com>
2022-04-09 23:17:03 -0300

    Filtragem de dia para para os cards blog

---
+++ api/get-date.js
@@ -0,0 +1,23 @@
+function getToday() {
+    var cards = document.getElementsByClassName('card-blog');
+    var datas = document.getElementsByClassName('p-data-card');
+
+    var dateNow = new Date();
+
+    for (let i = 0; i < cards.length; i++) {
+        var data = datas[i].outerText.split('/')
+
+        console.log(cards)
+
+        if (!(parseInt(data[0]) == dateNow.getDate() && parseInt(data[1]) == dateNow.getMonth() + 1 && parseInt(data[2]) == dateNow.getFullYear())) {
+            cards[i].classList.add("invisible")
+            cards[i].classList.remove("visible")
+            console.log("teste1")
+
+        } else {
+            cards[i].classList.add("visible")
+            cards[i].classList.remove("invisible")
+            console.log("teste2")
+        }
+    }
+}
