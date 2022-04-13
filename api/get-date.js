bf3c4203ae50dea600e27c0f6743ea73db524476
Gustavo Vieira <guga9055@gmail.com>
2022-04-13 01:08:56 -0300

    Filtragem do card-blog mês finalizada

--- api/get-date.js
+++ api/get-date.js
@@ -1,23 +1,33 @@
-function getToday() {
-    var cards = document.getElementsByClassName('card-blog');
-    var datas = document.getElementsByClassName('p-data-card');
+var cards = document.getElementsByClassName('card-blog');
+var datas = document.getElementsByClassName('p-data-card');
 
-    var dateNow = new Date();
+var dateNow = new Date();
 
+function getMonth() {
     for (let i = 0; i < cards.length; i++) {
         var data = datas[i].outerText.split('/')
 
-        console.log(cards)
-
-        if (!(parseInt(data[0]) == dateNow.getDate() && parseInt(data[1]) == dateNow.getMonth() + 1 && parseInt(data[2]) == dateNow.getFullYear())) {
+        if (!(parseInt(data[1]) == dateNow.getMonth() + 1 && parseInt(data[2]) == dateNow.getFullYear())) {
             cards[i].classList.add("invisible")
             cards[i].classList.remove("visible")
-            console.log("teste1")
 
         } else {
             cards[i].classList.add("visible")
             cards[i].classList.remove("invisible")
-            console.log("teste2")
+        }
+    }
+}
+
+function getToday() {
+    for (let i = 0; i < cards.length; i++) {
+        var data = datas[i].outerText.split('/')
+
+        if (!(parseInt(data[0]) == dateNow.getDate() && parseInt(data[1]) == dateNow.getMonth() + 1 && parseInt(data[2]) == dateNow.getFullYear())) {
+            cards[i].classList.add("invisible")
+            cards[i].classList.remove("visible")
+        } else {
+            cards[i].classList.add("visible")
+            cards[i].classList.remove("invisible")
         }
     }
 }
