01d904f5f151b4f9106cf9d27139d7ccde1a4eb3
Gustavo Vieira <guga9055@gmail.com>
2022-04-15 16:23:01 -0300

    Modal e JS do modal date picker

---
+++ api/selecionar-horario.js
@@ -0,0 +1,7 @@
+const teste2 = document.getElementById("container-horarios-id");
+const teste = teste2.getElementsByTagName("input");
+
+teste.addEventListener('click', function onClick() {
+    y.style.backgroundColor = 'salmon';
+    y.style.color = 'white';
+});
