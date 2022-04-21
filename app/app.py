1e43a774450d05e2e8af1626665ac9a7844b76b3
Lucas Nunes <64300982+kilerhg@users.noreply.github.com>
2022-04-21 20:43:55 -0300

    Fix Imports Index, About, Footer, Header

--- app/app.py
+++ app/app.py
@@ -2,16 +2,13 @@ from flask import Flask, render_template, request, redirect, url_for, flash, jso
 
 from user import bp as bp_user
 from main import bp as bp_main
-from billing import bp as bp_billing
 from box import bp as bp_box
 from blog import bp as bp_blog
 
 app = Flask(__name__)
 
-
 app.register_blueprint(bp_user)
 app.register_blueprint(bp_main)
-app.register_blueprint(bp_billing)
 app.register_blueprint(bp_box)
 app.register_blueprint(bp_blog)
 
